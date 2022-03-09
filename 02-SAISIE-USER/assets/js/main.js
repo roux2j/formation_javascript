3fcd3570405123feb013b765a0d58dc1b1765ad9
roux2j <roux2.j@orange.fr>
2022-03-09 15:16:43 +0100

    ajout du répertoir 2

--- 02-SAISIE-USER/assets/js/main.js
+++ 02-SAISIE-USER/assets/js/main.js
@@ -1,51 +1,47 @@
-//*====================================
-//* Les saisies utilisateurs en JS
-//*====================================
+// ==============================
+// LES SAISIES UTILISATEURS EN JS
+// ==============================
 
+// Rappel sur l'utilisation de la console
+// --------------------------------------
+console.log("Permet d'afficher une journalisation dans la console");
+console.warn("Attention ceci affiche un avertissement");
+console.error("Attention ceci affiche une erreur");
 
-//* Rappel sur l'utilisation de la console
-//*----------------------------------------
-//*console.log("permet d'afficher une journalisation dans la console");
-//*console.warn(3/0, "division dans l'ensemble des entiers");
-//*console.error("attention ceci affiche un erreur");
+// Pour afficher des informations sous forme d'alert --> alert
+alert("Bonjour JavaScript depuis la boite d'alert"); // alert() <==> window.alert()
 
+// Pour dialoguer avec le user --> confirm
+let reponse = confirm("Voulez-vous confirmer votre prénom ?");
+console.log(reponse); // confirm() ==> window.confirm()
 
+// Saisie user --> prompt()
+let saisie = prompt("Entrez votre âge");
+console.log(saisie);
+console.log(typeof saisie);
 
-//*Pour afficher des informations sous forme d'alert ---> alert
-//*alert ("bonjour javascript depuis la boite d'alerte"); //alerte () <==> window.alert
+// saisie = Number(saisie);
+saisie = saisie * (-1);
+console.log(saisie);
+console.log(typeof saisie);
 
-//*pour dialoguer avec le user --> confirm
-//*---------------------------------------
-//*let reponse = confirm ("voulez-vous confirmer votre prénom ?"); //*confirm() ==> window.alert
-//*console.log(reponse); // confirm () ==> window.confirm()
+// TOUT CE QUE VOUS SAISISSEZ DANS LE PROMPT N'EST QUE CHAINE DE CARACTERES
 
+let prenom = prompt("Entrez votre prenom");
+let age = Number(prompt("Entrez votre âge"));
 
-//*toute saisie pour JS  dans le prompt est un string ( chaine de caractère)
+console.log("Bonjour " + prenom + ", vous avez " + age + " ans");
+alert("Bonjour " + prenom + ", vous avez " + age + " ans");
+console.log('Bonjour ' + prenom + ', vous avez ' + age + ' ans');
+console.log("Bonjour " + prenom + ', vous avez ' + age + " ans");
 
+console.log("Dimanche j'irai au \"Japon\""); // On utilise le caractere \ d'echappement
+console.log('Dimanche j\'irai au "Japon"');
 
+console.log(`Dimanche j'irai au "Japon"`); // Template de String ou String Template
 
-//* saisie user -> prompt()
-//*let saisie = prompt("Entrez votre âge"); 
-//*console.log(saisie);
-//*console.log(typeof saisie);
+console.log("Bonjour " + prenom + ", vous avez " + age + " ans");
+console.log(`Bonjour ${prenom}, vous avez ${age} ans`);
 
-
-
-//*saisie = Number(saisie);
-//*saisie =  saisie * (1); //*transforme en numbre aussi grace à la multiplication
-//*console.log(saisie);
-//*console.log(typeof saisie);
-
-
-
-let prenom= prompt("Entrez votre prenom");
-let age= prompt("entrez votre âge");
-
-//*console.log("bonjour" + prenom + "vous avez" + age + "ans");
-console.log('bonjour ' + prenom + ', vous avez ' + age + 'ans');
-
-
-
-
-
-
+let fin = "Fin de cette partie du cours";
+console.log(fin);
